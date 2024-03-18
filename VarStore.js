f3b72e4cef8d375d2a1ca56954e88ee414111b79
Hiren12308 <shahhiru123@gmail.com>
2024-03-18 11:42:17 -0700

    add JavaScript to store variable

---
+++ VarStore.js
@@ -0,0 +1,8 @@
+function VarStore() {
+    //var f1 = document.getElementById("A1").classList.toggle("show");
+    //alert(document.getElementById("A1").selectedIndex);
+    var container = document.getElementById('A1');
+    var numberOfDivs = container.children.length;
+    var length = container.childNodes.length;
+alert(length);
+}
